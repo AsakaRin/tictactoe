e63b5cca6cfeeb0ea8ceb25410ae8901e3ba9328
AsakaRin <AsakaRin192@gmail.com>
2019-12-03 20:54:33 +0700

    last version1

--- application.py
+++ application.py
@@ -4,6 +4,17 @@ from tempfile import mkdtemp
 
 app = Flask(__name__)
 
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
